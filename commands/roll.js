91841bb51b7e3ea5f4c8a66826d856ac1aee8bc4
tet <carloskinoko@gmail.com>
2023-01-02 17:46:42 -0300

    new nick novo

--- commands/roll.js
+++ commands/roll.js
@@ -3,8 +3,8 @@ module.exports = {
   execute(mushroom, message, args) {
     args = args.join(' ').split('d');
     //if (args[0] == isNaN()) return message.channel.send('Você não conseguiu rolar um dado...');
-    quantidade = args[0], dados = args[1];
-    if(quantidade>='201' || dados>='256') return;
+    quantidade = args[0], dados = args[1];₢₢
+    if(quantidade>='201' || dados>='256') return;₢c₢c₢ccc₢₢
     var rolagem = Math.floor(Math.random()*dados);
     message.channel.send(quantidade*rolagem+1);
     console.log(args);
